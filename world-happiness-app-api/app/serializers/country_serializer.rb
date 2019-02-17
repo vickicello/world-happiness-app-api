f2510e7ba7d1bbb7b9896581203d7759df281899
vickicello <vicki.cello@gmail.com>
2019-02-17 16:35:08 -0500

    create serializers for country and comment

---
+++ world-happiness-app-api/app/serializers/country_serializer.rb
@@ -0,0 +1,3 @@
+class CountrySerializer < ActiveModel::Serializer
+  attributes :id, :country, :happiness_rank, :happiness_score, :whisker_high, :whisker_low, :economy, :family, :health, :freedom, :generosity, :trust, :dystopia_residual, :img_url
+end
